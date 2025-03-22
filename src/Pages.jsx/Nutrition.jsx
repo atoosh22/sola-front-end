a259d24f10a861d62ff2d8bd1f4e7373546b1f2f
atoosh22 <olaad22@gmail.com>
2025-03-22 21:44:14 +0300

    add cards

--- src/Pages.jsx/Nutrition.jsx
+++ src/Pages.jsx/Nutrition.jsx
@@ -5,7 +5,8 @@ import plantbaseddiet  from '../assets/img/plantbaseddiet.jpg'
 import paleodiet  from '../assets/img/paleodiet .jpg'
 import intermmitentfasting  from '../assets/img/intermmitentfasting.jpg'
 import Dashdiet  from '../assets/img/Dashdiet.jpg'
-import proteins from '../assets/icons/proteins.png'
+import protien from '../assets/icons/protien.png'
+import fats from '../assets/icons/fats.png'
 import React from 'react'
 
 const Nutrition = () => {
@@ -38,7 +39,7 @@ const Popular=() =>{
    <h3 className='text-xl font-bold my-4'>Mediterranean Diet</h3>
    <p>Emphasizes plant foods, olive oil, fish and <br/> moderate wine consuption.Associated with <br/>heart health and longevity</p>
   
-    <div className='flex gap-4 font-semi-bold my-6'>
+    <div className='flex gap-3 font-normal my-6'>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Heart-Health</a>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Anti-inflammontor</a>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Sustainable</a>
@@ -49,7 +50,7 @@ const Popular=() =>{
 <img className='w-full rounded-lg' src={katagonicdiet} alt="" />
    <h3 className='text-xl font-bold my-4'>Ketogenic Diet</h3>
    <p>Very low-carb, hight-fat diet that shifts the <br/> body  into ketosis. popular for weigh  loss and <br/> metabolic health</p>
-   <div className='flex gap-4 font-semi-bold my-6'>
+   <div className='flex gap-3 font-semi-bold my-6'>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Weigh lose</a>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Blood suger control</a>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>High Fat</a>
@@ -124,21 +125,58 @@ const Understand=()=>{
    </div>
 
    {/* cards */}
- <div className="grid grid-cols1 md:grid-cols-3">
- <div className="car1 shadow-lg bg-gray-100 pl-10">
-  <img className='w-10' src={proteins} alt="" />
- <h4 className='font-bold'>proteins</h4>
+ <div className="grid grid-cols1 md:grid-cols-3 gap-7 my-14 mx-6">
+  {/* car1 */}
+ <div className="car1 shadow-lg bg-gray-100 pl-10 my-6 py-4  w-full">
+  <img className='w-10' src={protien} alt="" />
+ <h4 className='font-bold my-2'>proteins</h4>
   <p>Essential for building and reparing tissues, immune<br/> function ,enzyme production. </p>
   <h4 className='my-5 text-xl text-[#44B75E]'>Good Sources</h4>
  
 
-  <p>•learn more</p>
+  <p><span className='text-[#44B75E] pr-2'>•</span>Lean meats and poultry</p>
+  <p><span className='text-[#44B75E] pr-2'>•</span>Fish and seafood</p>
+  <p><span className='text-[#44B75E] pr-2'>•</span>Eggs and dairy</p>
+  <p><span className='text-[#44B75E] pr-2'>•</span>Legumes and tofu</p>
+  <p><span className='text-[#44B75E] pr-2'>•</span>Nuts and seeds</p>
+ </div>
+ {/* car2 */}
+
+ <div className="car1 shadow-lg bg-gray-100 pl-10 my-6 py-4  w-full">
+  <img className='w-12' src={fats} alt="" />
+ <h4 className='font-bold my-2'>Carbohydrates</h4>
+  <p>Primary enargy source for the body , especially the brain <br/> and during high-intensity exercise </p>
+  <h4 className='my-5 text-xl text-[#44B75E]'>Good Sources</h4>
+ 
+
+  <p><span className='text-[#44B75E] pr-2'>•</span>Whole grains </p>
+  <p><span className='text-[#44B75E] pr-2'>•</span>Fruits and vegetables</p>
+  <p><span className='text-[#44B75E] pr-2'>•</span>Legumes and beans</p>
+  <p><span className='text-[#44B75E] pr-2'>•</span>Sweet Potatoes</p>
+  <p><span className='text-[#44B75E] pr-2'>•</span>Quinoa and oats</p>
  </div>
 
+ {/* card3 */}
+
+ <div className="car1 shadow-lg bg-gray-100 pl-10 my-6 py-4  w-full">
+  <img className='w-12' src={fats} alt="" />
+ <h4 className='font-bold my-2'>Fats</h4>
+  <p>Essential for building and reparing tissues, immune<br/> function ,enzyme production. </p>
+  <h4 className='my-5 text-xl text-[#44B75E]'>Good Sources</h4>
+ 
+
+  <p><span className='text-[#44B75E] pr-2'>•</span>Avacados</p>
+  <p><span className='text-[#44B75E] pr-2'>•</span>olive oil and olives</p>
+  <p><span className='text-[#44B75E] pr-2'>•</span>Nuts and seeds</p>
+  <p><span className='text-[#44B75E] pr-2'>•</span>fatty fish (salmon)</p>
+  <p><span className='text-[#44B75E] pr-2'>•</span>Eggs and full-fat yogurt</p>
+ </div>
 
  </div>
     </>
   )
 }
+
+// End cards
 
 export default Nutrition;
