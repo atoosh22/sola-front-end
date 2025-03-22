a919295e885095b61566ac4de3737ab499839535
atoosh22 <olaad22@gmail.com>
2025-03-22 17:08:08 +0300

    add cards

--- src/Pages.jsx/Nutrition.jsx
+++ src/Pages.jsx/Nutrition.jsx
@@ -5,6 +5,7 @@ import plantbaseddiet  from '../assets/img/plantbaseddiet.jpg'
 import paleodiet  from '../assets/img/paleodiet .jpg'
 import intermmitentfasting  from '../assets/img/intermmitentfasting.jpg'
 import Dashdiet  from '../assets/img/Dashdiet.jpg'
+import proteins from '../assets/icons/proteins.png'
 import React from 'react'
 
 const Nutrition = () => {
@@ -17,6 +18,7 @@ const Nutrition = () => {
    </div>
 
    <Popular/>
+   <Understand/>
   </>
   )
 }
@@ -29,7 +31,7 @@ const Popular=() =>{
    <h3 className=' text-2xl font-[Roboto]  font-bold'>Popular Diet Types</h3>
    <p className=''>Explore different dietary approaches and find what works best for your body lifesyle</p>
    </div>
-   <div className="grid grid-cols-1 md:grid-cols-3 gap-5 m-3 my-10 ">
+   <div className="grid grid-cols-1 md:grid-cols-3 gap-5 m-3 my-15 ">
     {/* img 1 */}
 <div className="img1">
 <img className='w-full rounded-lg' src={mediterranean} alt="" />
@@ -69,9 +71,9 @@ const Popular=() =>{
 {/* img4 */}
 
 <div className="img4">
-<img className='w-full rounded-lg' src={paleodiet} alt="" />
+<img className='w-full rounded-lg mt-20' src={paleodiet} alt="" />
    <h3 className='text-xl font-bold my-4'>Paleo Diet</h3>
-   <p>Based on foods presumed  to available  to <br/> paleolithic human. Excludes  grains, dairy, <br/> and   processed foods</p>
+   <p>Based on foods presumed  to available  to <br/> paleolithic human. Excludes  grains, dairy <br/> and   processed foods</p>
    <div className='flex gap-4 font-semi-bold my-6'>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Whole foods</a>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Grain-free</a>
@@ -81,9 +83,9 @@ const Popular=() =>{
 
 {/* img5 */}
 <div className="img5">
-<img className='w-full rounded-lg' src={intermmitentfasting} alt="" />
-   <h3 className='text-xl font-bold my-4'>Paleo Diet</h3>
-   <p>Based on foods presumed  to available  to <br/> paleolithic human. Excludes  grains, dairy, <br/> and   processed foods</p>
+<img className='w-full rounded-lg mt-20' src={intermmitentfasting} alt="" />
+   <h3 className='text-xl font-bold my-4'>Intermmitent Fasting</h3>
+   <p>Cycling between pariods of eating and fasting <br/> Various methods  include  16/8,5:2 and <br/> alternate -day fasting.</p>
    <div className='flex gap-4 font-semi-bold my-6'>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Whole foods</a>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Grain-free</a>
@@ -94,9 +96,9 @@ const Popular=() =>{
 {/* img6 */}
 
 <div className="img6">
-<img className='w-full rounded-lg' src={Dashdiet} alt="" />
-   <h3 className='text-xl font-bold my-4'>Paleo Diet</h3>
-   <p>Based on foods presumed  to available  to <br/> paleolithic human. Excludes  grains, dairy, <br/> and   processed foods</p>
+<img className='w-full rounded-lg mt-20' src={Dashdiet} alt="" />
+   <h3 className='text-xl font-bold my-4'>DASH Diet</h3>
+   <p>Design to prevent and lower high  blood <br/> pressute.Emphasizes fruits,  vegetables, whole <br/> grains, and low-fat dairy.</p>
    <div className='flex gap-4 font-semi-bold my-6'>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Whole foods</a>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Grain-free</a>
@@ -111,5 +113,32 @@ const Popular=() =>{
 };
 
 //End popular section
+
+
+const Understand=()=>{
+  return(
+    <>
+   <div className='text-center'>
+   <h2 className='text-2xl font-[Roboto]  font-bold' >Understanding Mocronutrients</h2>
+   <p>The tree mocronutrients-protein, corbohydrates, and fats each-play vitel roles in your Health</p>
+   </div>
+
+   {/* cards */}
+ <div className="grid grid-cols1 md:grid-cols-3">
+ <div className="car1 shadow-lg bg-gray-100 pl-10">
+  <img className='w-10' src={proteins} alt="" />
+ <h4 className='font-bold'>proteins</h4>
+  <p>Essential for building and reparing tissues, immune<br/> function ,enzyme production. </p>
+  <h4 className='my-5 text-xl text-[#44B75E]'>Good Sources</h4>
+ 
+
+  <p>•learn more</p>
+ </div>
+
+
+ </div>
+    </>
+  )
+}
 
 export default Nutrition;
