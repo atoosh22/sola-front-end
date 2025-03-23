37461a89aa0ef59dd20faf1bcdcb35b549dee0ad
atoosh22 <olaad22@gmail.com>
2025-03-23 22:12:14 +0300

    testing

--- src/Pages.jsx/Nutrition.jsx
+++ src/Pages.jsx/Nutrition.jsx
@@ -18,7 +18,7 @@ const Nutrition = () => {
   <>
    <div className='bg-[#44B75E] text-white text-center py-15'>
    <h2 className='text-4xl font-[Roboto] font-bold'>Nutrition & Diet Guide</h2>
-   <p className='font-[Roboto]'>Discover the Science of nutrition, explore different diet types. and learn how to create<br/> blanced meal plans for optical health</p>
+   <p className='font-[Roboto] font-semi-bold'>Discover the Science of nutrition, explore different diet types. and learn how to create<br/> blanced meal plans for optical health</p>
    <button className='bg-white rounded-full text-[#44B75E] px-4 py-2 mt-4 font-[Roboto] cursor-pointer'>Explore Diet</button>
    </div>
 
@@ -36,14 +36,14 @@ const Popular=() =>{
     <>
    <div className='text-center mt-10'>
    <h3 className=' text-2xl font-[Roboto]  font-bold'>Popular Diet Types</h3>
-   <p className=''>Explore different dietary approaches and find what works best for your body lifesyle</p>
+   <p className='font-light font-[Roboto]'>Explore different dietary approaches and find what works best for your body lifesyle</p>
    </div>
    <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mx-5  my-15 ">
     {/* img 1 */}
 <div className="img1">
 <img className='w-full rounded-lg' src={mediterranean} alt="" />
-   <h3 className='text-xl font-bold my-4'>Mediterranean Diet</h3>
-   <p>Emphasizes plant foods, olive oil, fish and <br/> moderate wine consuption.Associated with <br/>heart health and longevity</p>
+   <h3 className='text-xl font-bold my-4 font-[Roboto]'>Mediterranean Diet</h3>
+   <p className='font-light font-[Roboto]'>Emphasizes plant foods, olive oil, fish and <br/> moderate wine consuption.Associated with <br/>heart health and longevity</p>
   
     <div className='flex gap-3 font-normal my-6'>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Heart-Health</a>
@@ -54,8 +54,8 @@ const Popular=() =>{
 {/* img 2 */}
 <div className="img2">
 <img className='w-full rounded-lg' src={katagonicdiet} alt="" />
-   <h3 className='text-xl font-bold my-4'>Ketogenic Diet</h3>
-   <p>Very low-carb, hight-fat diet that shifts the <br/> body  into ketosis. popular for weigh  loss and <br/> metabolic health</p>
+   <h3 className='text-xl font-bold my-4 font-[Roboto]'>Ketogenic Diet</h3>
+   <p className='font-light font-[Roboto]'>Very low-carb, hight-fat diet that shifts the <br/> body  into ketosis. popular for weigh  loss and <br/> metabolic health</p>
    <div className='flex gap-3 font-semi-bold my-6'>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Weigh lose</a>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Blood suger control</a>
@@ -66,8 +66,8 @@ const Popular=() =>{
 
 <div className="img3">
 <img className='w-full rounded-lg' src={plantbaseddiet} alt="" />
-   <h3 className='text-xl font-bold my-4'>Plant-Based Diet</h3>
-   <p>Focuses on foods derived from p;amts with <br/> limited or no animal products. supports<br/> environmental sustainability</p>
+   <h3 className='text-xl font-bold my-4 font-[Roboto]'>Plant-Based Diet</h3>
+   <p className='font-light font-[Roboto]'>Focuses on foods derived from p;amts with <br/> limited or no animal products. supports<br/> environmental sustainability</p>
    <div className='flex gap-4 font-semi-bold my-6'>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Eco-Friendly</a>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>High Fiber</a>
@@ -79,8 +79,8 @@ const Popular=() =>{
 
 <div className="img4">
 <img className='w-full rounded-lg mt-20' src={paleodiet} alt="" />
-   <h3 className='text-xl font-bold my-4'>Paleo Diet</h3>
-   <p>Based on foods presumed  to available  to <br/> paleolithic human. Excludes  grains, dairy <br/> and   processed foods</p>
+   <h3 className='text-xl font-bold my-4 font-[Roboto]'>Paleo Diet</h3>
+   <p className='font-light font-[Roboto]'>Based on foods presumed  to available  to <br/> paleolithic human. Excludes  grains, dairy <br/> and   processed foods</p>
    <div className='flex gap-4 font-semi-bold my-6'>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Whole foods</a>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Grain-free</a>
@@ -91,8 +91,8 @@ const Popular=() =>{
 {/* img5 */}
 <div className="img5">
 <img className='w-full rounded-lg mt-20' src={intermmitentfasting} alt="" />
-   <h3 className='text-xl font-bold my-4'>Intermmitent Fasting</h3>
-   <p>Cycling between pariods of eating and fasting <br/> Various methods  include  16/8,5:2 and <br/> alternate -day fasting.</p>
+   <h3 className='text-xl font-bold my-4 font-[Roboto]'>Intermmitent Fasting</h3>
+   <p className='font-light font-[Roboto]'>Cycling between pariods of eating and fasting <br/> Various methods  include  16/8,5:2 and <br/> alternate -day fasting.</p>
    <div className='flex gap-4 font-semi-bold my-6'>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Whole foods</a>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Grain-free</a>
@@ -105,7 +105,7 @@ const Popular=() =>{
 <div className="img6">
 <img className='w-full rounded-lg mt-20' src={Dashdiet} alt="" />
    <h3 className='text-xl font-bold my-4'>DASH Diet</h3>
-   <p>Design to prevent and lower high  blood <br/> pressute.Emphasizes fruits,  vegetables, whole <br/> grains, and low-fat dairy.</p>
+   <p className='font-light font-[Roboto]'>Design to prevent and lower high  blood <br/> pressute.Emphasizes fruits,  vegetables, whole <br/> grains, and low-fat dairy.</p>
    <div className='flex gap-4 font-semi-bold my-6'>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Whole foods</a>
   <a href="#" className='bg-[#d8ede2] px-2 py-1 rounded-full'>Grain-free</a>
@@ -127,7 +127,7 @@ const Understand=()=>{
     <>
    <div className='text-center'>
    <h2 className='text-2xl font-[Roboto]  font-bold' >Understanding Mocronutrients</h2>
-   <p>The tree mocronutrients-protein, corbohydrates, and fats each-play vitel roles in your Health</p>
+   <p className='font-light font-[Roboto]'>The tree mocronutrients-protein, corbohydrates, and fats each-play vitel roles in your Health</p>
    </div>
 
    {/* cards */}
@@ -135,47 +135,47 @@ const Understand=()=>{
   {/* car1 */}
  <div className="car1 shadow-lg bg-gray-100 pl-10 my-6 py-4  w-full">
   <img className='w-10' src={protien} alt="" />
- <h4 className='font-bold my-2'>proteins</h4>
-  <p>Essential for building and reparing tissues, immune<br/> function ,enzyme production. </p>
-  <h4 className='my-5 text-xl text-[#44B75E]'>Good Sources</h4>
+ <h4 className='font-bold my-2 font-[Roboto]'>proteins</h4>
+  <p className='font-light font-[Roboto]'>Essential for building and reparing tissues, immune<br/> function ,enzyme production. </p>
+  <h4 className='my-5 text-xl text-[#44B75E] font-[Roboto]'>Good Sources</h4>
  
 
-  <p><span className='text-[#44B75E] pr-2'>•</span>Lean meats and poultry</p>
-  <p><span className='text-[#44B75E] pr-2'>•</span>Fish and seafood</p>
-  <p><span className='text-[#44B75E] pr-2'>•</span>Eggs and dairy</p>
-  <p><span className='text-[#44B75E] pr-2'>•</span>Legumes and tofu</p>
-  <p><span className='text-[#44B75E] pr-2'>•</span>Nuts and seeds</p>
+  <p className='font-light font-[Roboto]'><span className='text-[#44B75E] pr-2'>•</span>Lean meats and poultry</p>
+  <p className='font-light font-[Roboto]'><span className='text-[#44B75E] pr-2'>•</span>Fish and seafood</p>
+  <p className='font-light font-[Roboto]'><span className='text-[#44B75E] pr-2'>•</span>Eggs and dairy</p>
+  <p className='font-light font-[Roboto]'><span className='text-[#44B75E] pr-2'>•</span>Legumes and tofu</p>
+  <p className='font-light font-[Roboto]'><span className='text-[#44B75E] pr-2'>•</span>Nuts and seeds</p>
  </div>
  {/* car2 */}
 
  <div className="car1 shadow-lg bg-gray-100 pl-10 my-6 py-4  w-full">
   <img className='w-12' src={fats} alt="" />
- <h4 className='font-bold my-2'>Carbohydrates</h4>
-  <p>Primary enargy source for the body , especially the brain <br/> and during high-intensity exercise </p>
-  <h4 className='my-5 text-xl text-[#44B75E]'>Good Sources</h4>
+ <h4 className='font-bold my-2 font-[]Roboto'>Carbohydrates</h4>
+  <p className='font-light font-[Roboto]'>Primary enargy source for the body , especially the brain <br/> and during high-intensity exercise </p>
+  <h4 className='my-5 text-xl text-[#44B75E] font-[Roboto]'>Good Sources</h4>
  
 
-  <p><span className='text-[#44B75E] pr-2'>•</span>Whole grains </p>
-  <p><span className='text-[#44B75E] pr-2'>•</span>Fruits and vegetables</p>
-  <p><span className='text-[#44B75E] pr-2'>•</span>Legumes and beans</p>
-  <p><span className='text-[#44B75E] pr-2'>•</span>Sweet Potatoes</p>
-  <p><span className='text-[#44B75E] pr-2'>•</span>Quinoa and oats</p>
+  <p className='font-light font-[Roboto]'><span className='text-[#44B75E] pr-2'>•</span>Whole grains </p>
+  <p className='font-light font-[Roboto]'><span className='text-[#44B75E] pr-2'>•</span>Fruits and vegetables</p>
+  <p className='font-light font-[Roboto]'><span className='text-[#44B75E] pr-2'>•</span>Legumes and beans</p>
+  <p className='font-light font-[Roboto]'><span className='text-[#44B75E] pr-2'>•</span>Sweet Potatoes</p>
+  <p className='font-light font-[Roboto]'><span className='text-[#44B75E] pr-2'>•</span>Quinoa and oats</p>
  </div>
 
  {/* card3 */}
 
  <div className="car1 shadow-lg bg-gray-100 pl-10 my-6 py-4  w-full">
   <img className='w-12' src={fats} alt="" />
- <h4 className='font-bold my-2'>Fats</h4>
-  <p>Essential for building and reparing tissues, immune<br/> function ,enzyme production. </p>
-  <h4 className='my-5 text-xl text-[#44B75E]'>Good Sources</h4>
+ <h4 className='font-bold my-2 font-[Roboto]'>Fats</h4>
+  <p className='font-light font-[Roboto]'>Essential for building and reparing tissues, immune<br/> function ,enzyme production. </p>
+  <h4 className='my-5 text-xl text-[#44B75E] font-[Roboto]'>Good Sources</h4>
  
 
-  <p><span className='text-[#44B75E] pr-2'>•</span>Avacados</p>
-  <p><span className='text-[#44B75E] pr-2'>•</span>olive oil and olives</p>
-  <p><span className='text-[#44B75E] pr-2'>•</span>Nuts and seeds</p>
-  <p><span className='text-[#44B75E] pr-2'>•</span>fatty fish (salmon)</p>
-  <p><span className='text-[#44B75E] pr-2'>•</span>Eggs and full-fat yogurt</p>
+  <p className='font-light font-[Roboto]'><span className='text-[#44B75E] pr-2'>•</span>Avacados</p>
+  <p className='font-light font-[Roboto]'><span className='text-[#44B75E] pr-2'>•</span>olive oil and olives</p>
+  <p className='font-light font-[Roboto]'><span className='text-[#44B75E] pr-2'>•</span>Nuts and seeds</p>
+  <p className='font-light font-[Roboto]'><span className='text-[#44B75E] pr-2'>•</span>fatty fish (salmon)</p>
+  <p className='font-light font-[Roboto]'><span className='text-[#44B75E] pr-2'>•</span>Eggs and full-fat yogurt</p>
  </div>
 
  </div>
@@ -191,7 +191,7 @@ function Balanced(){
     {/* title */}
   <div className="title py-6">
   <h2 className='text-2xl font-[Roboto]  font-bold'>Balanced Meal Planing</h2>
-  <p>Create Nutritions, satisfying meals with these simple guidelines</p>
+  <p className='font-light font-[Roboto]'>Create Nutritions, satisfying meals with these simple guidelines</p>
   </div>
 
 {/* plate method */}
@@ -201,10 +201,10 @@ function Balanced(){
       <h2 className='py-4 font-bold font-[Roboto]'>The Plate method</h2>
       <img src={plantbaseddiet} alt="" />
     <div className="numbers my-7 ">
-    <h3 className='mt-3'><span className='bg-[#d8ede2] rounded-full text-center text-[#44B75E] px-2 py-1 m-4 '>1</span>Fill half your plate whith non-starchy vegetables</h3>
-    <h3 className='mt-3'><span className='bg-[#d8ede2] rounded-full  text-[#44B75E] px-2 py-1 m-4 ' >2</span>Fill one quarter with protein-checking, fish, tofo,</h3>
-    <h3 className='mt-3 mr-14 md:ml-6 '><span className='bg-[#d8ede2] rounded-full text-center text-[#44B75E] px-2 py-1 m-6  '>3</span>Fill one quarter with complex carbohydrates </h3>
-    <h3 className='mt-3'><span className='bg-[#d8ede2] rounded-full text-center text-[#44B75E] px-2 py-1 m-4 '>4</span>Add a small amout of health fat-olive oil avocado</h3>
+    <h3 className='mt-3 font-light font-[Roboto]'><span className='bg-[#d8ede2] rounded-full text-center text-[#44B75E] px-2 py-1 m-4 '>1</span>Fill half your plate whith non-starchy vegetables</h3>
+    <h3 className='mt-3 font-light font-[Roboto]'><span className='bg-[#d8ede2] rounded-full  text-[#44B75E] px-2 py-1 m-4 ' >2</span>Fill one quarter with protein-checking, fish, tofo,</h3>
+    <h3 className='mt-3 font-light font-[Roboto] mr-14 md:ml-6 '><span className='bg-[#d8ede2] rounded-full text-center text-[#44B75E] px-2 py-1 m-6  '>3</span>Fill one quarter with complex carbohydrates </h3>
+    <h3 className='mt-3 font-light font-[Roboto]'><span className='bg-[#d8ede2] rounded-full text-center text-[#44B75E] px-2 py-1 m-4 '>4</span>Add a small amout of health fat-olive oil avocado</h3>
 
 
     </div>
@@ -278,7 +278,7 @@ function Frequently(){
     </div>
 
     <div className=" grid grid-cols-1 md:grid-cols-2 gap-7 ">
-      <div className="card1 shadow-lg border-2 border-red-50  p-10">
+      <div className="card1 shadow-lg   p-10">
         <h4 className='font-semi-bold font-4xl font-[Roboto]'>How many calories should I eat per day?</h4>
         <p className='font-light font-[Roboto]'>Caloric needs very based on age , sex , weight, height and activity level . for general  guidance ,  
           about women typically need 1,600-2400 colories per day. while adult man need 2,000-3,000 colories  
@@ -288,9 +288,17 @@ function Frequently(){
 
           <div className="card2 shadow-lg border-2 border-red-50 p-10 ">
         <h5 className='font-semi-bold font-2xl font-[Roboto]'>What is the different B/w simple and complex corbohydrates?</h5>
-        <p>Simple carbs like suger and white flour(RDA)
+        <p className='font-light font-[Roboto]'>Simple carbs like suger and white flour(RDA)
            are quickly digedted  and can spike blood suger complex. 
            carbs like whole graind and vegetables contain fiber digest more slowly, and provide sustained energy with our dramatic blood suger fluctuations.</p>
+      </div>
+
+      {/* card3 */}
+
+      <div className="card3 shadow-lg border-2 border-red-50 p-10 ">
+        <h5 className='font-semi-bold font-2xl font-[Roboto]'>How much protein do I need?</h5>
+        <p className='font-light font-[Roboto]'> The Recommended  Dietary Allowance  (RDA) is 0.8 grames per kilogram of body weight  However , 
+          Active individuals while older adults  and those looking to build muscle may benefit frm higher  intake  typicall 12:-2.3g/kg of body weigh</p>
       </div>
 
     </div>
